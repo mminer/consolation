daf1cca0ce381a4d32f88b8c3aabb3b4341b2110
Matthew Miner <matthew@matthewminer.com>
2014-04-13 11:12:58 -0400

    Wrap class in namespace. Closes #2.

--- Console.cs
+++ Console.cs
@@ -1,127 +1,130 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-/// <summary>
-/// A console to display Unity's debug logs in-game.
-/// </summary>
-public class Console : MonoBehaviour
+namespace Consolation
 {
-	struct Log
-	{
-		public string message;
-		public string stackTrace;
-		public LogType type;
-	}
-
 	/// <summary>
-	/// The hotkey to show and hide the console window.
+	/// A console to display Unity's debug logs in-game.
 	/// </summary>
-	public KeyCode toggleKey = KeyCode.BackQuote;
-
-	List<Log> logs = new List<Log>();
-	Vector2 scrollPosition;
-	bool show;
-	bool collapse;
-
-	// Visual elements:
-
-	static readonly Dictionary<LogType, Color> logTypeColors = new Dictionary<LogType, Color>()
+	public class Console : MonoBehaviour
 	{
-		{ LogType.Assert, Color.white },
-		{ LogType.Error, Color.red },
-		{ LogType.Exception, Color.red },
-		{ LogType.Log, Color.white },
-		{ LogType.Warning, Color.yellow },
-	};
-
-	const int margin = 20;
-
-	Rect windowRect = new Rect(margin, margin, Screen.width - (margin * 2), Screen.height - (margin * 2));
-	Rect titleBarRect = new Rect(0, 0, 10000, 20);
-	GUIContent clearLabel = new GUIContent("Clear", "Clear the contents of the console.");
-	GUIContent collapseLabel = new GUIContent("Collapse", "Hide repeated messages.");
-
-	void OnEnable ()
-	{
-		Application.RegisterLogCallback(HandleLog);
-	}
-
-	void OnDisable ()
-	{
-		Application.RegisterLogCallback(null);
-	}
-
-	void Update ()
-	{
-		if (Input.GetKeyDown(toggleKey)) {
-			show = !show;
-		}
-	}
-
-	void OnGUI ()
-	{
-		if (!show) {
-			return;
+		struct Log
+		{
+			public string message;
+			public string stackTrace;
+			public LogType type;
 		}
 
-		windowRect = GUILayout.Window(123456, windowRect, ConsoleWindow, "Console");
-	}
+		/// <summary>
+		/// The hotkey to show and hide the console window.
+		/// </summary>
+		public KeyCode toggleKey = KeyCode.BackQuote;
 
-	/// <summary>
-	/// A window that displayss the recorded logs.
-	/// </summary>
-	/// <param name="windowID">Window ID.</param>
-	void ConsoleWindow (int windowID)
-	{
-		scrollPosition = GUILayout.BeginScrollView(scrollPosition);
+		List<Log> logs = new List<Log>();
+		Vector2 scrollPosition;
+		bool show;
+		bool collapse;
 
-			// Iterate through the recorded logs.
-			for (int i = 0; i < logs.Count; i++) {
-				var log = logs[i];
+		// Visual elements:
 
-				// Combine identical messages if collapse option is chosen.
-				if (collapse) {
-					var messageSameAsPrevious = i > 0 && log.message == logs[i - 1].message;
+		static readonly Dictionary<LogType, Color> logTypeColors = new Dictionary<LogType, Color>()
+		{
+			{ LogType.Assert, Color.white },
+			{ LogType.Error, Color.red },
+			{ LogType.Exception, Color.red },
+			{ LogType.Log, Color.white },
+			{ LogType.Warning, Color.yellow },
+		};
 
-					if (messageSameAsPrevious) {
-						continue;
+		const int margin = 20;
+
+		Rect windowRect = new Rect(margin, margin, Screen.width - (margin * 2), Screen.height - (margin * 2));
+		Rect titleBarRect = new Rect(0, 0, 10000, 20);
+		GUIContent clearLabel = new GUIContent("Clear", "Clear the contents of the console.");
+		GUIContent collapseLabel = new GUIContent("Collapse", "Hide repeated messages.");
+
+		void OnEnable ()
+		{
+			Application.RegisterLogCallback(HandleLog);
+		}
+
+		void OnDisable ()
+		{
+			Application.RegisterLogCallback(null);
+		}
+
+		void Update ()
+		{
+			if (Input.GetKeyDown(toggleKey)) {
+				show = !show;
+			}
+		}
+
+		void OnGUI ()
+		{
+			if (!show) {
+				return;
+			}
+
+			windowRect = GUILayout.Window(123456, windowRect, ConsoleWindow, "Console");
+		}
+
+		/// <summary>
+		/// A window that displayss the recorded logs.
+		/// </summary>
+		/// <param name="windowID">Window ID.</param>
+		void ConsoleWindow (int windowID)
+		{
+			scrollPosition = GUILayout.BeginScrollView(scrollPosition);
+
+				// Iterate through the recorded logs.
+				for (int i = 0; i < logs.Count; i++) {
+					var log = logs[i];
+
+					// Combine identical messages if collapse option is chosen.
+					if (collapse) {
+						var messageSameAsPrevious = i > 0 && log.message == logs[i - 1].message;
+
+						if (messageSameAsPrevious) {
+							continue;
+						}
 					}
+
+					GUI.contentColor = logTypeColors[log.type];
+					GUILayout.Label(log.message);
 				}
 
-				GUI.contentColor = logTypeColors[log.type];
-				GUILayout.Label(log.message);
-			}
+			GUILayout.EndScrollView();
 
-		GUILayout.EndScrollView();
+			GUI.contentColor = Color.white;
 
-		GUI.contentColor = Color.white;
+			GUILayout.BeginHorizontal();
 
-		GUILayout.BeginHorizontal();
+				if (GUILayout.Button(clearLabel)) {
+					logs.Clear();
+				}
 
-			if (GUILayout.Button(clearLabel)) {
-				logs.Clear();
-			}
+				collapse = GUILayout.Toggle(collapse, collapseLabel, GUILayout.ExpandWidth(false));
 
-			collapse = GUILayout.Toggle(collapse, collapseLabel, GUILayout.ExpandWidth(false));
+			GUILayout.EndHorizontal();
 
-		GUILayout.EndHorizontal();
+			// Allow the window to be dragged by its title bar.
+			GUI.DragWindow(titleBarRect);
+		}
 
-		// Allow the window to be dragged by its title bar.
-		GUI.DragWindow(titleBarRect);
-	}
-
-	/// <summary>
-	/// Records a log from the log callback.
-	/// </summary>
-	/// <param name="message">Message.</param>
-	/// <param name="stackTrace">Trace of where the message came from.</param>
-	/// <param name="type">Type of message (error, exception, warning, assert).</param>
-	void HandleLog (string message, string stackTrace, LogType type)
-	{
-		logs.Add(new Log() {
-			message = message,
-			stackTrace = stackTrace,
-			type = type,
-		});
+		/// <summary>
+		/// Records a log from the log callback.
+		/// </summary>
+		/// <param name="message">Message.</param>
+		/// <param name="stackTrace">Trace of where the message came from.</param>
+		/// <param name="type">Type of message (error, exception, warning, assert).</param>
+		void HandleLog (string message, string stackTrace, LogType type)
+		{
+			logs.Add(new Log() {
+				message = message,
+				stackTrace = stackTrace,
+				type = type,
+			});
+		}
 	}
 }

3891cfa9cbbd5ebc6ebb66f765313617dca332da
Matthew Miner <matthew@matthewminer.com>
2018-02-20 14:11:29 -0800

    Clean up to use more common C# code style
    Change function spacing style, use type inference when possible, remove
    excessive and mostly unhelpful function documentation comments.

--- Console.cs
+++ Console.cs
@@ -71,43 +71,42 @@ namespace Consolation
 
         #region MonoBehaviour Messages
 
-        void OnDisable ()
+        void OnDisable()
         {
             Application.logMessageReceived -= HandleLog;
         }
 
-        void OnEnable ()
+        void OnEnable()
         {
             Application.logMessageReceived += HandleLog;
         }
 
-        void OnGUI ()
+        void OnGUI()
         {
-            if (!isVisible) {
+            if (!isVisible)
+            {
                 return;
             }
 
             windowRect = GUILayout.Window(123456, windowRect, DrawWindow, windowTitle);
         }
 
-        void Update ()
+        void Update()
         {
-            if (Input.GetKeyDown(toggleKey)) {
+            if (Input.GetKeyDown(toggleKey))
+            {
                 isVisible = !isVisible;
             }
 
-            if (shakeToOpen && Input.acceleration.sqrMagnitude > shakeAcceleration) {
+            if (shakeToOpen && Input.acceleration.sqrMagnitude > shakeAcceleration)
+            {
                 isVisible = true;
             }
         }
 
         #endregion
 
-        /// <summary>
-        /// Displays a log entry with a badge indicating the number of times it's been consecutively recorded.
-        /// <summary>
-        /// <param name="log">Log information.</param>
-        void DrawCollapsedLog (Log log)
+        void DrawCollapsedLog(Log log)
         {
             GUILayout.BeginHorizontal();
 
@@ -118,55 +117,50 @@ namespace Consolation
             GUILayout.EndHorizontal();
         }
 
-        /// <summary>
-        /// Displays a log entry with separate labels for consecutive recordings.
-        /// </summary>
-        /// <param name="log">Log information.</param>
-        void DrawExpandedLog (Log log)
+        void DrawExpandedLog(Log log)
         {
-            for (int i = 0; i < log.count; i += 1) {
+            for (var i = 0; i < log.count; i += 1)
+            {
                 GUILayout.Label(log.message);
             }
         }
 
-        /// <summary>
-        /// Displays a log entry.
-        /// </summary>
-        /// <param name="log">Log information.</param>
-        void DrawLog (Log log)
+        void DrawLog(Log log)
         {
             GUI.contentColor = logTypeColors[log.type];
 
-            if (isCollapsed) {
+            if (isCollapsed)
+            {
                 DrawCollapsedLog(log);
-            } else {
+            }
+            else
+            {
                 DrawExpandedLog(log);
             }
         }
 
-        /// <summary>
-        /// Displays a scrollable list of logs.
-        /// </summary>
-        void DrawLogList ()
+        void DrawLogList()
         {
             scrollPosition = GUILayout.BeginScrollView(scrollPosition);
 
             // Used to determine height of accumulated log labels.
             GUILayout.BeginVertical();
 
-                IEnumerable<Log> visibleLogs = logs.Where(IsLogVisible);
+                var visibleLogs = logs.Where(IsLogVisible);
 
-                foreach (Log log in visibleLogs) {
+                foreach (Log log in visibleLogs)
+                {
                     DrawLog(log);
                 }
 
             GUILayout.EndVertical();
-            Rect innerScrollRect = GUILayoutUtility.GetLastRect();
+            var innerScrollRect = GUILayoutUtility.GetLastRect();
             GUILayout.EndScrollView();
-            Rect outerScrollRect = GUILayoutUtility.GetLastRect();
+            var outerScrollRect = GUILayoutUtility.GetLastRect();
 
             // If we're scrolled to bottom now, guarantee that it continues to be in next cycle.
-            if (Event.current.type == EventType.Repaint && IsScrolledToBottom(innerScrollRect, outerScrollRect)) {
+            if (Event.current.type == EventType.Repaint && IsScrolledToBottom(innerScrollRect, outerScrollRect))
+            {
                 ScrollToBottom();
             }
 
@@ -174,20 +168,19 @@ namespace Consolation
             GUI.contentColor = Color.white;
         }
 
-        /// <summary>
-        /// Displays options for filtering and changing the logs list.
-        /// </summary>
-        void DrawToolbar ()
+        void DrawToolbar()
         {
             GUILayout.BeginHorizontal();
 
-                if (GUILayout.Button(clearLabel)) {
+                if (GUILayout.Button(clearLabel))
+                {
                     logs.Clear();
                 }
 
-                foreach (LogType logType in Enum.GetValues(typeof(LogType))) {
-                    bool currentState = logTypeFilters[logType];
-                    string label = logType.ToString();
+                foreach (LogType logType in Enum.GetValues(typeof(LogType)))
+                {
+                    var currentState = logTypeFilters[logType];
+                    var label = logType.ToString();
                     logTypeFilters[logType] = GUILayout.Toggle(currentState, label, GUILayout.ExpandWidth(false));
                     GUILayout.Space(20);
                 }
@@ -197,11 +190,7 @@ namespace Consolation
             GUILayout.EndHorizontal();
         }
 
-        /// <summary>
-        /// Displays a window that lists the recorded logs.
-        /// </summary>
-        /// <param name="windowID">Window ID.</param>
-        void DrawWindow (int windowID)
+        void DrawWindow(int windowID)
         {
             DrawLogList();
             DrawToolbar();
@@ -210,72 +199,56 @@ namespace Consolation
             GUI.DragWindow(titleBarRect);
         }
 
-        /// <summary>
-        /// Finds the last recorded log.
-        /// <summary>
-        /// <returns>The last recorded log, or null if the list is empty.</returns>
-        Log? GetLastLog ()
+        Log? GetLastLog()
         {
-            if (logs.Count == 0) {
+            if (logs.Count == 0)
+            {
                 return null;
             }
 
             return logs.Last();
         }
 
-        /// <summary>
-        /// Records a log from the log callback.
-        /// </summary>
-        /// <param name="message">Message.</param>
-        /// <param name="stackTrace">Trace of where the message came from.</param>
-        /// <param name="type">Type of message (error, exception, warning, assert).</param>
-        void HandleLog (string message, string stackTrace, LogType type)
+        void HandleLog(string message, string stackTrace, LogType type)
         {
-            Log log = new Log {
+            var log = new Log {
                 count = 1,
                 message = message,
                 stackTrace = stackTrace,
                 type = type,
             };
 
-            Log? lastLog = GetLastLog();
-            bool isDuplicateOfLastLog = lastLog.HasValue && log.Equals(lastLog.Value);
+            var lastLog = GetLastLog();
+            var isDuplicateOfLastLog = lastLog.HasValue && log.Equals(lastLog.Value);
 
-            if (isDuplicateOfLastLog) {
+            if (isDuplicateOfLastLog)
+            {
                 // Replace previous log with incremented count instead of adding a new one.
                 log.count = lastLog.Value.count + 1;
                 logs[logs.Count - 1] = log;
-            } else {
+            }
+            else
+            {
                 logs.Add(log);
                 TrimExcessLogs();
             }
         }
 
-        /// <summary>
-        /// Determines whether the user has chosen to hide the given log.
-        /// </summary>
-        /// <param name="log">Log information.</param>
-        /// <returns>Whether the log hasn't been filtered out.</returns>
-        bool IsLogVisible (Log log)
+        bool IsLogVisible(Log log)
         {
             return logTypeFilters[log.type];
         }
 
-        /// <summary>
-        /// Determines whether the scroll view is scrolled to the bottom.
-        /// </summary>
-        /// <param name="innerScrollRect">Rect surrounding scroll view content.</param>
-        /// <param name="outerScrollRect">Scroll view container.</param>
-        /// <returns>Whether scroll view is scrolled to bottom.</returns>
-        bool IsScrolledToBottom (Rect innerScrollRect, Rect outerScrollRect)
+        bool IsScrolledToBottom(Rect innerScrollRect, Rect outerScrollRect)
         {
-            float innerScrollHeight = innerScrollRect.height;
+            var innerScrollHeight = innerScrollRect.height;
 
             // Take into account extra padding added to the scroll container.
-            float outerScrollHeight = outerScrollRect.height - GUI.skin.box.padding.vertical;
+            var outerScrollHeight = outerScrollRect.height - GUI.skin.box.padding.vertical;
 
             // If contents of scroll view haven't exceeded outer container, treat it as scrolled to bottom.
-            if (outerScrollHeight > innerScrollHeight) {
+            if (outerScrollHeight > innerScrollHeight)
+            {
                 return true;
             }
 
@@ -283,26 +256,22 @@ namespace Consolation
             return Mathf.Approximately(innerScrollHeight, scrollPosition.y + outerScrollHeight);
         }
 
-        /// <summary>
-        /// Moves the scroll view down so that the last log is visible.
-        /// </summary>
-        void ScrollToBottom ()
+        void ScrollToBottom()
         {
             scrollPosition = new Vector2(0, Int32.MaxValue);
         }
 
-        /// <summary>
-        /// Removes old logs that exceed the maximum number allowed if log count restriction is enabled.
-        /// </summary>
-        void TrimExcessLogs ()
+        void TrimExcessLogs()
         {
-            if (!restrictLogCount) {
+            if (!restrictLogCount)
+            {
                 return;
             }
 
-            int amountToRemove = logs.Count - maxLogCount;
+            var amountToRemove = logs.Count - maxLogCount;
 
-            if (amountToRemove <= 0) {
+            if (amountToRemove <= 0)
+            {
                 return;
             }
 
@@ -320,7 +289,7 @@ namespace Consolation
         public string stackTrace;
         public LogType type;
 
-        public bool Equals (Log log)
+        public bool Equals(Log log)
         {
             return message == log.message && stackTrace == log.stackTrace && type == log.type;
         }
